2660d73d839184d57469e5573de361ee06d23913
fedorovra <fedorov.r.a@hotmail.com>
2020-01-30 14:01:41 +0500

    1234

--- 2.py
+++ 2.py
@@ -1,19 +1,27 @@
 def parse(num):
-    rim_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    prev = 0
-    for i in num[::-1]:
-        if rim_num[i] < prev and prev != 0:
-            result = result - rim_num[i]
+    rim_num: dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result: int = 0
+    prev: int = 0
+    for counter in num[::-1]:
+        if rim_num[counter] < prev != 0:
+            result = result - rim_num[counter]
         else:
-            result = result + rim_num[i]
-        prev = rim_num[i]
+            result = result + rim_num[counter]
+        prev = rim_num[counter]
     return result
 
 
 if __name__ == "__main__":
     while True:
         r_num: str = input('please enter R num: ')
-        print(f'A repr is {parse(r_num)}')
+        error: bool = True
+        for i in r_num:
+            if i not in "IXLCDM":
+                error = False
+                break
+        if error:
+            print(f'A repr is {parse(r_num)}')
+        else:
+            print(f'Input error')
 else:
     pass
